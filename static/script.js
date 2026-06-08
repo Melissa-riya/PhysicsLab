65235a579869f5fac92dfe08ce6b7cd0b490aeac
Melissa-riya <melissaveyn@gmail.com>
2026-06-08 14:12:51 +0300

    крнчscript.js

--- static/script.js
+++ static/script.js
@@ -381,7 +381,7 @@ function drawScheme() {
     
     ctx.fillStyle = '#2c3e50';
     ctx.font = 'bold 10px Arial';
-    ctx.fillText(titleText, W/2, 225);
+    ctx.fillText(titleText, W/2, 235);
     
     // Сохраняем координаты для кликов
     window.rulerClickAreas = [];
